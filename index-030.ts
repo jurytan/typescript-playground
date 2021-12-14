dc7c4d491eab59b10b529e224fb240e94c7e9f57
jnguyen7410 <jurytann@grubhub.com>
2021-12-13 23:44:15 -0500

    Adding type narrowing

---
+++ index-030.ts
@@ -0,0 +1,14 @@
+function formatValue(value: string | number) {
+    // Write your code here
+    // we're using type narrowing here to use type specific logic
+    if (typeof value == 'string') {
+      console.log(value.toLowerCase());
+    }
+    if (typeof value == 'number') {
+      console.log(value.toFixed(2));
+    }
+  
+  }
+  
+  formatValue('Hiya');
+  formatValue(42);
