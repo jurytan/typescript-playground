efad6df5acddfd0a30e3500d0386a51ab8b17725
jnguyen7410 <jnguyen7410@gmail.com>
2021-12-15 22:13:33 -0500

    Added union type array

---
+++ index-032.ts
@@ -0,0 +1,23 @@
+// union types can also be used in arrays, which can help
+// with dealing with multiple formats of data types
+// i.e. different date formats
+function formatListings(listings : (string | number)[]) {
+    return listings.map((listing) => {
+      if (typeof listing === 'string') {
+        return listing.toUpperCase();
+      }
+  
+      if (typeof listing === 'number') {
+        return `$${listing.toLocaleString()}`;
+      }
+    });
+  }
+  
+  const result = formatListings([
+    '123 Main St',
+    226800,
+    '580 Broadway Apt 4a',
+    337900,
+  ]);
+  
+  console.log(result);
